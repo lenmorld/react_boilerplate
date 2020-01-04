e78ac68d057ee298f1ea0bf09cccfa899f6547c4
lenny <lenmorld@gmail.com>
2020-01-04 15:58:06 -0500

    Revert "webpack setup"
    This reverts commit 0463b5a4d5b41fa3b190978236f56c364f2a8878.

--- app/models/lesson.rb
+++
@@ -1,4 +0,0 @@
-class Lesson < ApplicationRecord
-    has_many :comments, dependent: :destroy
-    validates :title, presence: true, length: { minimum: 5 }
-end
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,11 @@
 var path = require("path");
 
-const es6SourcePath = `${__dirname}/public/javascripts`;
-const outputPath = `${__dirname}/public/dist`;
-
 module.exports = {
 	mode: "development",
-	context: __dirname,
-	// entry: "./app/index.jsx",
-	entry: `${es6SourcePath}/index.jsx`,
+	context: path.join(__dirname, "./"),
+	entry: "./app/index.jsx",
 	output: {
-        // path: path.join(__dirname, "public"),
-        path: outputPath,
+		path: path.join(__dirname, "public"),
 		filename: "bundle.js"
 	},
 	devtool: 'cheap-module-eval-source-map',
@@ -23,17 +18,15 @@ module.exports = {
 				test: /\.jsx?$/,
 				loader: "babel-loader",
 				exclude: /node_modules/,
-				// include: path.join(__dirname, "app"),
-				include: es6SourcePath,
+				include: path.join(__dirname, "app"),
 				options: {
 					presets: ["@babel/react"]
 				}
 			},
 		]
-    },
+	},
 	devServer: {
-		// contentBase: "./public",
-		contentBase: outputPath,
+		contentBase: "./public",
 		port: 4001,
 		// compress: true,
 	}
